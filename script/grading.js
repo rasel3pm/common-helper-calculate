3f055cfdb55c25533d6e3f1e4af1f86b8d20c380
Rasel <mdraselkibria11@gmail.com>
2023-10-16 23:16:08 +0600

    updated

--- script/grading.js
+++ script/grading.js
@@ -2,15 +2,16 @@ let handleGradeButton = document.getElementById("buttonGrade");
 let show = document.getElementById("show");
 handleGradeButton.addEventListener("click", function (e) {
   e.preventDefault();
-  const totalLength = 27; // cm
+  // const totalLength = 27; // cm
   let Parsent = document.getElementById("Parsent").value;
+  let dicliarCm = document.getElementById("Dicliar-Cm").value;
   if (Parsent === "") {
     alert("Please Enter Parsent");
     return false;
   }
 
   if (!isNaN(Parsent)) {
-    let result = (Parsent / 100) * totalLength;
+    let result = (Parsent / 100) * dicliarCm;
     show.innerText = "Result: " + result.toFixed(2) + " cm ";
   } else {
     show.innerText = "Please enter valid values.";
